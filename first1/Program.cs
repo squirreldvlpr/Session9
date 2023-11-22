03232b71c9014784e8ed2be3b593e6d07f079c55
squirreldvlpr <saleh8zarei@outlook.com>
2023-11-22 08:11:37 +0330

    horizental space added

--- first1/Program.cs
+++ first1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 for (int i = 0; i < 5; i++)
 {
-    Console.WriteLine($"Saleh and Mohammad time: {i+1}");
+    Console.WriteLine($"Saleh and Mohammad\tRound: {i+1}");
 }
