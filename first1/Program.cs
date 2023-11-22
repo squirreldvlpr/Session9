af95dbed181d05176f178b38d2ff9a6963a6311d
squirreldvlpr <saleh8zarei@outlook.com>
2023-11-22 08:10:04 +0330

    project created

--- first1/Program.cs
+++ first1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Saleh and Mohammad time: {i+1}");
+}
