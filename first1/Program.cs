b235e5a58cfeb5ad002e87bf01ae60f900938d81
rezalte <reza.talei.66@gmail.com>
2023-11-22 08:47:20 +0330

    //

--- first1/Program.cs
+++ first1/Program.cs
@@ -2,5 +2,5 @@
 for (int i = 0; i < 5; i++)
 {
     Console.WriteLine($"Saleh and Mohammad and Reza\tRound: {i+1}");
-    // DADASH YE USERNAME SADETAR BEZAR :-)
+    // DADASH YE USERNAME SADETAR BEZAR :-) DAMET GARM
 }
