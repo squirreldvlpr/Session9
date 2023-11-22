1f1eaff35f38d019f3e79a81ed9fbd6c541312dd
squirreldvlpr <saleh8zarei@outlook.com>
2023-11-22 08:16:45 +0330

    chnaged for to inline for

--- first1/Program.cs
+++ first1/Program.cs
@@ -1,5 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-for (int i = 0; i < 5; i++)
-{
-    Console.WriteLine($"Saleh and Mohammad\tRound: {i+1}");
-}
+for (int i = 0; i < 5; Console.WriteLine($"Saleh and Mohammad\tRound: {i + 1}"), i++) ;
+
